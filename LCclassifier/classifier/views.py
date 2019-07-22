08480840842ca6453783bd6aec3c3cf759cc4546
Shivanshu Ayachi <shivanshuayachi@gmail.com>
2019-07-22 16:22:01 +0530

    Update view for uploading photos

--- LCclassifier/classifier/views.py
+++ LCclassifier/classifier/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .models import Image
+from .forms import PictureForm
+
+def UploadPhoto(request):
+
+    image_object = Image()
+    context={}
+    uploaded = False
+
+    if request.method == "POST":
+        
+        form = PictureForm(request.POST, request.FILES, instance = image_object)
+        if form.is_valid():
+            image_object = form.save(commit = False)
+            image_object.save()
+            uploaded = True
+            context['image_url'] = image_object.image.url
+
+    form = PictureForm(instance = image_object)
+    
+    context['form']=form
+    context['uploaded']=uploaded
+
+    return render(request, template_name = 'classifier/photo_upload.html', context=context)
